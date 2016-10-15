48ce87d30f8975e014f2de8ca644b1d5251b7e4f
Edgar Paz Moreno <Edgar@MacBook-Air-de-Edgar-3.local>
2016-10-14 22:06:17 -0500

    nav, jumbo, thumbnails y sliders

--- APP/app/js/main.js
+++ APP/app/js/main.js
@@ -1,4 +1,10 @@
+'use strict';
 
-$(document). ready (function (){
-console.log('hello world');
-})
+$(document). ready (function () {
+	var carousel = $('#carousel-example-generic');
+	console.log('hello world');
+
+	carousel.carousel({
+		interval: 2000
+	});
+});
